93539da30703084a0435b7d98476e17918e6fa6a
RajeenKhadka <rajeenkhadka315@gmail.com>
2024-12-11 23:44:04 -0500

    updated React

---
+++ react/src/components/Article.jsx
@@ -0,0 +1,40 @@
+function Article() {
+  return (
+    <main>
+      <section>
+        <article>
+          <time datetime="2020-11-12">11/12/20</time>
+          <h2>On the Street in Brooklyn</h2>
+          <img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            aspernatur perferendis dolorum et in fugit animi excepturi.
+            Consectetur nihil quaerat dignissimos. Tempora nesciunt odit
+            cupiditate corporis doloremque facilis architecto dicta.
+          </p>
+          <aside>
+            <h3>Continues...</h3>
+          </aside>
+        </article>
+      </section>
+      <section>
+        <article>
+          <time datetime="2020-11-11">11/11/20</time>
+          <h2>Vintage in Vogue</h2>
+          <img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            ratione delectus a nostrum voluptatem error eaque repudiandae
+            perspiciatis quo, non dolorem? Modi ipsum adipisci sit! Odit optio
+            asperiores modi. Quam.
+          </p>
+          <aside>
+            <h3>Continues...</h3>
+          </aside>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Article;
---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <footer>
+      <nav aria-label="Main Navigation" role="navigation" class="bottom-nav">
+        <ul>
+          <li>
+            <a href="/">Women's</a>
+          </li>
+          <li>
+            <a href="/">Men's</a>
+          </li>
+          <li>
+            <a href="/">On the Street</a>
+          </li>
+          <li>
+            <a href="/">The Catwalk</a>
+          </li>
+          <li>
+            <a href="/">AdWatch</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Tips</a>
+          </li>
+        </ul>
+        <p>&copy; 2013 Valet Industries, Inc</p>
+      </nav>
+    </footer>
+  );
+}
+
+export default Footer;
